e0dc0c3bf3c1bc51896eff2eda2f203af3d60e1e
Ed <eduards.torba@gmail.com>
2015-03-30 16:16:24 +0100

    Continent, Country, Neighbour continents models

---
+++ Manhattan/Models/NeightbourContinent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Manhattan.Models
+{
+    public class NeightbourContinent
+    {
+        public int ContinentID { get; set; }
+        public string Name { get; set; }
+
+        public NeightbourContinent(int ContinentID, string Name)
+        {
+            this.ContinentID = ContinentID;
+            this.Name = Name;
+        }
+    }
+}
