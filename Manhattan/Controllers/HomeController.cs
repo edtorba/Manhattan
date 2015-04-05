8b469ff641d642305571acb3552c7a8fdd704078
Ed <eduards.torba@gmail.com>
2015-04-05 15:51:31 +0100

    HomeController Pages

--- Manhattan/Controllers/HomeController.cs
+++ Manhattan/Controllers/HomeController.cs
@@ -13,5 +13,65 @@ namespace Manhattan.Controllers
         {
             return View();
         }
+
+        // GET: GetContinents
+        public ActionResult GetContinents()
+        {
+            return View();
+        }
+
+        // GET: GetContinentsID
+        public ActionResult GetContinentsID()
+        {
+            return View();
+        }
+
+        // GET: PostContinents
+        public ActionResult PostContinents()
+        {
+            return View();
+        }
+
+        // GET: PutContinentsID
+        public ActionResult PutContinentsID()
+        {
+            return View();
+        }
+
+        // GET: DeleteContinentsID
+        public ActionResult DeleteContinentsID()
+        {
+            return View();
+        }
+
+        // GET: GetCountries
+        public ActionResult GetCountries()
+        {
+            return View();
+        }
+
+        // GET: GetCountriesID
+        public ActionResult GetCountriesID()
+        {
+            return View();
+        }
+
+        // GET: PostCountries
+        public ActionResult PostCountries()
+        {
+            return View();
+        }
+
+        // GET: PutCountriesID
+        public ActionResult PutCountriesID()
+        {
+            return View();
+        }
+
+        // GET: DeleteCountriesID
+        public ActionResult DeleteCountriesID()
+        {
+            return View();
+        }
     }
 }
